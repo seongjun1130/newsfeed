009ad7f47e0fff6ef21fe914fe9531039655a29a
CHOSEONGJUN <lionjojo@naver.com>
2024-10-21 12:17:45 +0900

    init : Create Entity - 프로젝트 필요 공통 Entity 생성

---
+++ src/main/java/com/sparta/newsfeedproject/domain/comment/entity/Comment.java
@@ -0,0 +1,30 @@
+package com.sparta.newsfeedproject.domain.comment.entity;
+
+import com.sparta.newsfeedproject.domain.audit.Auditable;
+import com.sparta.newsfeedproject.domain.member.entity.Member;
+import com.sparta.newsfeedproject.domain.news.entity.News;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@Entity
+@Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Comment extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "comment", nullable = false, length = 100)
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "news_id")
+    private News news;
+}
