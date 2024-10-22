5a92169d52b575a7c62281c2820c3483257ff60e
anewsdev <anewsdev@gmail.com>
2024-10-22 11:28:53 +0900

    fix : update Friend 1. 불필요 변수 제거 2. 예외처리 메서드로 정리

--- src/main/java/com/sparta/newsfeedproject/domain/friend/dto/FriendRequestDto.java
+++ src/main/java/com/sparta/newsfeedproject/domain/friend/dto/FriendRequestDto.java
@@ -8,6 +8,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class FriendRequestDto {
-    private Long requesterId;
     private Long receiverId;
 }
--- src/main/java/com/sparta/newsfeedproject/domain/friend/service/FriendService.java
+++ src/main/java/com/sparta/newsfeedproject/domain/friend/service/FriendService.java
@@ -3,7 +3,6 @@ package com.sparta.newsfeedproject.domain.friend.service;
 import com.sparta.newsfeedproject.domain.exception.CustomException;
 import com.sparta.newsfeedproject.domain.exception.eunm.ErrorCode;
 import com.sparta.newsfeedproject.domain.friend.entity.FriendRequest;
-import com.sparta.newsfeedproject.domain.friend.enumPac.FriendRequestStatus;
 import com.sparta.newsfeedproject.domain.friend.repository.FriendRepository;
 import com.sparta.newsfeedproject.domain.friend.repository.FriendRequestRepository;
 import com.sparta.newsfeedproject.domain.member.entity.Member;
@@ -26,33 +25,35 @@ public class FriendService {
         Member receiver = memberRepository.findById(receiverId)
                 .orElseThrow(() -> new CustomException(ErrorCode.RECEIVER_NOT_FOUND));
 
-        // 자신에게 친구 요청을 보내는 경우 예외 처리
-        if (requesterId.equals(receiverId)) {
-            throw new CustomException(ErrorCode.CANNOT_FRIEND_YOURSELF);
-        }
-
-        // 이미 친구인 경우 예외 처리
-        boolean friendStatus = friendRepository.existsByMemberAndFriend(requester, receiver);
-        if (friendStatus) {
-            //이미 친구 상태
-            throw new CustomException(ErrorCode.ALREADY_FRIEND);
-        }
-
-        // 이미 요청된 경우 예외 처리 (처리되지 않은 요청 확인)
-        boolean requestStatus = friendRequestRepository.existsByRequesterAndReceiver(requester, receiver);
-        if (requestStatus) {
-            // 이미 요청 상태
-            throw new CustomException(ErrorCode.ALREADY_REQUEST);
-        }
-
-        // 상대방이 나에게 이미 친구 요청을 보낸 경우 (교차 요청 방지)
-        boolean isCrossRequestSent = friendRequestRepository.existsByRequesterAndReceiver(receiver, requester);
-        if (isCrossRequestSent) {
-            throw new CustomException(ErrorCode.ALREADY_REQUEST);
-        }
+        validateFriendRequest(requesterId, receiverId, requester, receiver);
 
         FriendRequest friendRequest = new FriendRequest();
         friendRequest.update(requester,receiver);
         friendRequestRepository.save(friendRequest);
+    }
+
+    private void validateFriendRequest(Long requesterId, Long receiverId, Member requester, Member receiver) {
+        // 1. 자신에게 친구 요청을 보내는 경우 예외 처리
+        if (requesterId.equals(receiverId)) {
+            throw new CustomException(ErrorCode.CANNOT_FRIEND_YOURSELF);
+        }
+
+        // 2. 이미 친구인 경우 예외 처리
+        boolean friendStatus = friendRepository.existsByMemberAndFriend(requester, receiver);
+        if (friendStatus) {
+            throw new CustomException(ErrorCode.ALREADY_FRIEND);
+        }
+
+        // 3. 이미 요청된 경우 예외 처리
+        boolean requestStatus = friendRequestRepository.existsByRequesterAndReceiver(requester, receiver);
+        if (requestStatus) {
+            throw new CustomException(ErrorCode.ALREADY_REQUEST);
+        }
+
+        // 4. 상대방이 이미 나에게 친구 요청을 보낸 경우 (교차 요청 방지)
+        boolean isCrossRequestSent = friendRequestRepository.existsByRequesterAndReceiver(receiver, requester);
+        if (isCrossRequestSent) {
+            throw new CustomException(ErrorCode.ALREADY_REQUEST);
+        }
     }
 }
