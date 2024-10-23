5208b2525d4a7bff889cc568fe3941db8bf5e87f
CHOSEONGJUN <lionjojo@naver.com>
2024-10-23 13:45:56 +0900

    feat : Remove Like News,Comment - 자신이 누른 좋아요 취소기능 추가

--- src/main/java/com/sparta/newsfeedproject/domain/like/repository/LikeRepository.java
+++ src/main/java/com/sparta/newsfeedproject/domain/like/repository/LikeRepository.java
@@ -3,8 +3,14 @@ package com.sparta.newsfeedproject.domain.like.repository;
 import com.sparta.newsfeedproject.domain.like.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
     boolean existsByMemberIdAndNewsId(Long memberId, Long newsId);
 
     boolean existsByMemberIdAndCommentId(Long memberId, Long commentId);
+
+    Optional<Like> findByMemberIdAndNewsId(Long memberId, Long newsId);
+
+    Optional<Like> findByMemberIdAndCommentId(Long memberId, Long commentId);
 }
