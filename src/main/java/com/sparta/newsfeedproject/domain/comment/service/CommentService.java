cf0557d6047f1ea5e626dddc747f5a8385bd01d4
CHOSEONGJUN <lionjojo@naver.com>
2024-10-24 11:27:16 +0900

    fix  : CommentId Mismatch - 댓글 ID가 멤버 ID 로 찾아가던 현상수정

--- src/main/java/com/sparta/newsfeedproject/domain/comment/service/CommentService.java
+++ src/main/java/com/sparta/newsfeedproject/domain/comment/service/CommentService.java
@@ -23,9 +23,9 @@ public class CommentService {
 
     @Transactional
     public CommentResponseDto createComment(CommentRequestDto commentRequestDto, Long memberId, Long newsId) {
-        Member member = memberRepository.findById(memberId).orElseThrow(()->new CustomException(ErrorCode.USER_NOT_FOUND));
-        News news = newsRepository.findById(newsId).orElseThrow(()->new CustomException(ErrorCode.NEWS_NOT_FOUND));
-        Comment comment = Comment.from(commentRequestDto, member,news);
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        News news = newsRepository.findById(newsId).orElseThrow(() -> new CustomException(ErrorCode.NEWS_NOT_FOUND));
+        Comment comment = Comment.from(commentRequestDto, member, news);
         Comment savedComment = commentRepository.save(comment);
         return CommentResponseDto.builder()
                 .id(comment.getId())
@@ -35,28 +35,30 @@ public class CommentService {
 
     @Transactional
     public CommentResponseDto updateComment(CommentRequestDto commentRequestDto, Long memberId, Long commentId) {
-        Member member = memberRepository.findById(memberId).orElseThrow(()->new CustomException(ErrorCode.USER_NOT_FOUND));
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
         Comment comment = commentRepository.findCommentById(commentId);
-        authorizedArticleAuthor(member,comment);
+        authorizedArticleAuthor(member, comment);
         comment.updatedata(commentRequestDto);
         return CommentResponseDto.builder()
                 .id(comment.getId())
                 .message("댓글이 수정되었습니다").build();
     }
+
     @Transactional
-    public CommentResponseDto deleteComment(Long memberId, Long commentId) {
-        Member member = memberRepository.findById(memberId).orElseThrow(()->new CustomException(ErrorCode.USER_NOT_FOUND));
+    public CommentResponseDto deleteComment(Long memberId, Long newsId, Long commentId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
         Comment comment = commentRepository.findCommentById(commentId);
-        authorizedArticleAuthor(member,comment);
+        authorizedArticleAuthor(member, comment);
         commentRepository.deleteById(commentId);
         return CommentResponseDto.builder()
                 .id(comment.getId())
                 .message("댓글이 삭제되었습니다").build();
     }
+
     private void authorizedArticleAuthor(Member member, Comment comment) {
-    if (!member.getId().equals(comment.getMember().getId())) {
-        throw new CustomException(ErrorCode.UNAUTHORIZED_ACTION);  // 권한이 없을 때 예외 발생
+        if (!member.getId().equals(comment.getMember().getId())) {
+            throw new CustomException(ErrorCode.UNAUTHORIZED_ACTION);  // 권한이 없을 때 예외 발생
+        }
     }
-}
 
 }
